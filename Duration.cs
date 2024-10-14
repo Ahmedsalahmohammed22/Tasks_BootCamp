c26e0a94dd92e3e53049b0c8e9b2f5e5c6c6993d
ahmed salah <81277940+Ahmedsalahmohammed22@users.noreply.github.com>
2024-10-14 23:53:16 +0300

    Task class lib and Task durationTask class lib and Task duration

---
+++ Duration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace University
+{
+    internal class Duration
+    {
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        public Duration(int hours , int minutes , int seconds)
+        {
+            this.Hours = hours;
+            this.Minutes = minutes;
+            this.Seconds = seconds;
+        }
+
+        public Duration(int totalSeconds)
+        {
+            this.Hours = totalSeconds / 3600;
+            this.Minutes = (totalSeconds % 3600) / 60 ;
+            this.Seconds = totalSeconds % 60;
+        }
+        public string getstring()
+        {
+            return $"Hours: {Hours} Minutes: {Minutes} Seconds: {Seconds}";
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using StudentLibrary;
+
+namespace University
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var student = new Student(1, "Ahmed", 23, "ahmed@gmail.com", "01011121505", "ST", "IT");
+            Console.WriteLine(student.DisplayInfo());
+
+            Duration D1 = new Duration(1, 10, 15);
+            D1.getstring();
+
+            Duration D2 = new Duration(3600);
+            Console.WriteLine( D2.getstring() );
+
+            Duration D3 = new Duration(666);
+            Console.WriteLine( D3.getstring() );
+        }
+    }
+}
