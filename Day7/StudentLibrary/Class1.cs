f473d55f65351e093d318ec62ee76e6bee932f63
ahmed salah <81277940+Ahmedsalahmohammed22@users.noreply.github.com>
2024-10-14 23:57:46 +0300

    organize files

---
+++ Day7/StudentLibrary/Class1.cs
@@ -0,0 +1,29 @@
+﻿namespace StudentLibrary
+{
+    public class Student
+    {
+        public int Id { get ; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Subject { get; set; }
+        public string Department { get; set; }
+
+        public Student(int id , string name , int age , string email , string phone , string subject , string department )
+        {
+            this.Id = id ;
+            this.Name = name;   
+            this.Age = age ;
+            this.Email = email ;
+            this.Phone = phone ;
+            this.Subject = subject;
+            this.Department = department ;
+        }
+
+        public string DisplayInfo()
+        {
+            return $"ID: {Id}\nName: {Name}\nAge: {Age}\nEmail: {Email}\nPhone: {Phone}\nSubject: {Subject}\nDepartment: {Department}";
+        }
+    }
+}
