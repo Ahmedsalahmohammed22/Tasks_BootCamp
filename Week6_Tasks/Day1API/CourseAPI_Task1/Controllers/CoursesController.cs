e2d31c36a6cdb9c9c3c6a7b23a13d0dbde37f5e6
ahmed salah <81277940+Ahmedsalahmohammed22@users.noreply.github.com>
2024-11-03 21:44:33 +0200

    API_D1_Tasks

---
+++ Week6_Tasks/Day1API/CourseAPI_Task1/Controllers/CoursesController.cs
@@ -0,0 +1,28 @@
+﻿using CourseAPI_Task1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CourseAPI_Task1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        static List<Course> crs = new List<Course>()
+        {
+           new  Course(){id=1,name="CS",duration=30,status=true},
+           new  Course(){id=2,name="SW",duration=45,status=true},
+           new  Course(){id=3,name="Network",duration=60,status=false},
+        };
+        [HttpGet]
+        public List<Course> GetAll()
+        {
+            return crs;
+        }
+        [HttpGet("{id}")]
+        public Course GetById(int id)
+        {
+            return crs.Where(c => c.id == id).SingleOrDefault();
+        }
+    }
+}
---
+++ Week6_Tasks/Day1API/CourseAPI_Task1/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseAPI_Task1.Models
+{
+    public class Course
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int duration { get; set; }
+        public bool status { get; set; }
+    }
+}
